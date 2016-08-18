559be713862de3ef29813937556c3a01048cacb9
Ian Mendoza Jaimes <ianmj2013@gmail.com>
2016-08-18 18:07:13 -0500

    numeros primos completo

--- numeros_primos/main.py
+++ numeros_primos/main.py
@@ -7,7 +7,7 @@ def main():
     archivo.close()
     seguir = True
     while seguir:
-        print("\n\nSelecciona el modo en que se ejecutara el programa: \n 1.- Automático \n 2.- Manual \n 3.- Salir")
+        print("\n\nSelecciona el modo en que se ejecutará el programa: \n 1.- Automático \n 2.- Manual \n 3.- Salir")
         try:
             seleccion = int(input())
             if seleccion > 0 and seleccion <= 2:
@@ -17,37 +17,58 @@ def main():
             else:
                 raise Exception()
         except Exception as e:
-            print("Por favor ingrese un dato valido.")
+            print("Por favor ingrese un dato válido.")
 
 
 def iniciar_programa(seleccion=1):
     numeros_primos = []
     numeros_primos_binarios = []
+    numero_ceros_unos = []
     n = 0
-
+    continuar = True
     archivo = open("primos.txt", "a")
 
-    if seleccion == 2:
-        n = ingresar_datos()
-    else:
-        n = int(random() * 1000)
-        print("\nFue seleccionado un n = ", n)
+    while continuar:
+        if seleccion == 2:
+            n = ingresar_datos("\nIngrese un número límite ( 0 < n <= 1000): ")
+        else:
+            n = int(random() * 1000)
+            print("\nFue seleccionado un n = ", n)
 
-    numeros_primos = encontrar_primos(numeros_primos, n)
-    numeros_primos_binarios = convertir_primos_a_binarios(numeros_primos, archivo)
+        numeros_primos = encontrar_primos(numeros_primos, n)
+        numeros_primos_binarios = convertir_primos_a_binarios(numeros_primos, archivo)
+        numero_ceros_unos = contar_ceros_unos(numeros_primos_binarios)
+
+        imprimir_ceros_unos(numero_ceros_unos, numeros_primos)
+
+        if seleccion == 1:
+            continuar = int(random() * 100) % 2
+        else:
+            eleccion = ingresar_datos("\n¿Desea ingresar otra n?  \n1.- Si \n2.- No \n")
+            if eleccion != 1:
+                continuar = False
 
     archivo.close()
 
 
-def ingresar_datos():
+def ingresar_datos(texto):
     while True:
         try:
-            dato_n = int(input("\nIngrese el número límite (desde 1 hasta 1000):"))
+            dato_n = int(input(texto))
             if dato_n > 0 and dato_n <= 1000:
                 return dato_n
             else:
                 raise Exception()
         except Exception as e:
-            print("Por favor ingrese un dato valido")
+            print("Por favor ingrese un dato válido")
+
+
+def imprimir_ceros_unos(numero_ceros_unos, numeros_primos):
+    contador = 0
+    print (numeros_primos)
+    print("{ numero primo, numero de unos }")
+    while contador < len(numeros_primos):
+        print("{",numeros_primos[contador], ",",numero_ceros_unos[contador][1],"}", end=", ")
+        contador += 1
 
 main()
--- numeros_primos/metodos.py
+++ numeros_primos/metodos.py
@@ -2,7 +2,9 @@ def encontrar_primos(numeros_primos, n):
     if n == 1:
         return numeros_primos
 
-    numeros_primos.append(2)    
+    if len(numeros_primos) == 0:
+        numeros_primos.append(2)
+        
     for x in range(2,n+1):
         es_primo = True
 
@@ -20,7 +22,25 @@ def encontrar_primos(numeros_primos, n):
 def convertir_primos_a_binarios(numeros_primos, archivo):
     numeros_primos_binarios = []
     for x in numeros_primos:
-        numeros_primos_binarios.append(int(bin(x)[2:]))
+        numeros_primos_binarios.append(bin(x)[2:])
         archivo.write(", " + bin(x)[2:])
 
     return numeros_primos_binarios
+
+
+def contar_ceros_unos(numeros_primos_binarios):
+    numero_ceros_unos = []
+    contador_ceros = 0
+    contador_unos = 0
+    for x in numeros_primos_binarios:
+        contador_unos = 0
+        contador_ceros = 0
+        for y in x:
+            if y == '0':
+                contador_ceros += 1
+            else:
+                contador_unos += 1
+
+        numero_ceros_unos.append([contador_ceros, contador_unos])
+
+    return numero_ceros_unos
